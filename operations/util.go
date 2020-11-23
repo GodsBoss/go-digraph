884d9f84b5f9bbab6f16907a8338a0eab1cd9b09
Timo Reitz <godsboss@gmx.de>
2020-11-23 18:17:31 +0100

    Add more information to InvertedNodeMapping().

--- operations/util.go
+++ operations/util.go
@@ -6,6 +6,12 @@ import (
 
 // InvertedNodeMapping returns an inverted node mapping. The map passed to this
 // function remains unchanged.
+// If the input map contains duplicate values, is is undefined which of them will
+// become key in the return value. For maps m containing every value just once,
+// InvertedNodeMapping(InvertedNodeMapping(m)) == m
+// holds true. In general, for an arbitrary map m,
+// len(InvertedNodeMapping(m)) <= len(m)
+// holds true.
 func InvertedNodeMapping(mapping map[digraph.Node]digraph.Node) map[digraph.Node]digraph.Node {
 	result := make(map[digraph.Node]digraph.Node)
 	for key := range mapping {
