67e89941e2f97878fcf7ea3684560146016f6416
Timo Reitz <godsboss@gmx.de>
2020-12-06 21:44:06 +0100

    Implement bijective node <-> string mapper.

---
+++ uniquemappers/string.go
@@ -0,0 +1,155 @@
+package uniquemappers
+
+import (
+	"fmt"
+
+	"github.com/GodsBoss/go-digraph"
+)
+
+// String maps nodes to strings bijectively.
+type String interface {
+	// Add maps node and a string value from the pair to each other. It errors if node
+	// or the string are mapped already, even if it is the same pair.
+	Add(pair StringNodePair) error
+
+	// Remove removes the connection between a node and a string. Returns an error
+	// if node or string (or both) are not mapped.
+	Remove(pair StringNodePair) error
+
+	// RemoveNode unmaps the given node, ignoring which string value it is mapped
+	// to. Returns an error if the node is not mapped.
+	RemoveNode(node digraph.Node) error
+
+	// RemoveString unmaps the given string, ignoring which node it is mapped to.
+	// Returns an error if the string is not mapped.
+	RemoveString(s string) error
+
+	// GetNodeFor returns the node for a string. It returns an error if no such
+	// node exists.
+	GetNodeFor(s string) (digraph.Node, error)
+
+	// GetStringFor returns the string for a node. It returns an error if no such
+	// string exists.
+	GetStringFor(node digraph.Node) (string, error)
+
+	// HasString checks wether the string s is mapped to a node.
+	HasString(s string) bool
+
+	// HasNode checks wether node is mapped to a string.
+	HasNode(node digraph.Node) bool
+
+	// Nodes returns all nodes this mapper contains in no particular order.
+	// The return value is free for modification.
+	Nodes() []digraph.Node
+
+	// Strings returns all strings this mapper contains in no particular order.
+	// The return value is free for modification.
+	Strings() []string
+}
+
+// StringNodePair represents a node and its corresponding string (usually some
+// form of ID, e.g. a UUID).
+type StringNodePair struct {
+	Node   digraph.Node
+	String string
+}
+
+// NewString returns a new unique mapper from nodes to strings (and vice versa).
+func NewString() String {
+	return &stringMapper{
+		nodeToString: make(map[digraph.Node]string),
+		stringToNode: make(map[string]digraph.Node),
+	}
+}
+
+type stringMapper struct {
+	nodeToString map[digraph.Node]string
+	stringToNode map[string]digraph.Node
+}
+
+func (m *stringMapper) Add(pair StringNodePair) error {
+	if m.HasNode(pair.Node) {
+		return fmt.Errorf("node already taken")
+	}
+	if m.HasString(pair.String) {
+		return fmt.Errorf("string already taken")
+	}
+	m.nodeToString[pair.Node] = pair.String
+	m.stringToNode[pair.String] = pair.Node
+	return nil
+}
+
+func (m *stringMapper) Remove(pair StringNodePair) error {
+	if !m.HasNode(pair.Node) || !m.HasString(pair.String) {
+		return fmt.Errorf("invalid pair")
+	}
+	m.remove(pair.Node, pair.String)
+	return nil
+}
+
+func (m *stringMapper) RemoveNode(node digraph.Node) error {
+	s, err := m.GetStringFor(node)
+	if err != nil {
+		return err
+	}
+	m.remove(node, s)
+	return nil
+}
+
+func (m *stringMapper) RemoveString(s string) error {
+	node, err := m.GetNodeFor(s)
+	if err != nil {
+		return err
+	}
+	m.remove(node, s)
+	return nil
+}
+
+// remove is called by the Remove* methods after checking wether removal is possible.
+func (m *stringMapper) remove(node digraph.Node, s string) {
+	delete(m.nodeToString, node)
+	delete(m.stringToNode, s)
+
+}
+
+func (m *stringMapper) GetNodeFor(s string) (digraph.Node, error) {
+	node, ok := m.stringToNode[s]
+	if !ok {
+		return nil, fmt.Errorf("string not found")
+	}
+	return node, nil
+}
+
+func (m *stringMapper) GetStringFor(node digraph.Node) (string, error) {
+	s, ok := m.nodeToString[node]
+	if !ok {
+		return "", fmt.Errorf("node not found")
+	}
+	return s, nil
+}
+
+func (m *stringMapper) HasString(s string) bool {
+	_, ok := m.stringToNode[s]
+	return ok
+}
+
+func (m *stringMapper) HasNode(node digraph.Node) bool {
+	_, ok := m.nodeToString[node]
+	return ok
+}
+
+func (m *stringMapper) Nodes() []digraph.Node {
+	nodes := make([]digraph.Node, 0, len(m.nodeToString))
+	for node := range m.nodeToString {
+		nodes = append(nodes, node)
+	}
+	return nodes
+}
+
+func (m *stringMapper) Strings() []string {
+	ss := make([]string, 0, len(m.stringToNode))
+	for s := range m.stringToNode {
+		ss = append(ss, s)
+	}
+	return ss
+}
---
+++ uniquemappers/string_test.go
@@ -0,0 +1,206 @@
+package uniquemappers_test
+
+import (
+	"github.com/GodsBoss/go-digraph"
+	"github.com/GodsBoss/go-digraph/uniquemappers"
+
+  "sort"
+	"testing"
+)
+
+func TestAddStringMappingErrs(t *testing.T) {
+	g := digraph.New()
+	n1, n2, n3 := g.NewNode(), g.NewNode(), g.NewNode()
+	mapper := uniquemappers.NewString()
+	mapper.Add(
+		uniquemappers.StringNodePair{
+			Node:   n1,
+			String: "foo",
+		},
+	)
+
+	testcases := map[string]struct {
+		node        digraph.Node
+		str         string
+		expectedErr bool
+	}{
+		"success":              {n2, "bar", false},
+		"fail (both exist)":    {n1, "foo", true},
+		"fail (node exists)":   {n1, "baz", true},
+		"fail (string exists)": {n3, "foo", true},
+	}
+
+	for name := range testcases {
+		testcase := testcases[name]
+
+		t.Run(
+			name,
+			func(t *testing.T) {
+				err := mapper.Add(
+					uniquemappers.StringNodePair{
+						Node:   testcase.node,
+						String: testcase.str,
+					},
+				)
+				if testcase.expectedErr && err == nil {
+					t.Error("expected error")
+				}
+				if err != nil && !testcase.expectedErr {
+					t.Errorf("expected no error, got %+v", err)
+				}
+			},
+		)
+	}
+}
+
+func TestStringMappingRemoval(t *testing.T) {
+	g := digraph.New()
+	n1, n2, n3 := g.NewNode(), g.NewNode(), g.NewNode()
+
+	pair1 := uniquemappers.StringNodePair{Node: n1, String: "1"}
+	pair2 := uniquemappers.StringNodePair{Node: n2, String: "2"}
+	pair3 := uniquemappers.StringNodePair{Node: n3, String: "3"}
+
+	mapper := uniquemappers.NewString()
+	mapper.Add(pair1)
+	mapper.Add(pair2)
+	mapper.Add(pair3)
+
+	testcases := map[string]struct {
+		f    func() error
+		pair uniquemappers.StringNodePair
+	}{
+		"pair 1": {
+			pair: pair1,
+			f: func() error {
+				return mapper.Remove(pair1)
+			},
+		},
+		"pair 2": {
+			pair: pair2,
+			f: func() error {
+				return mapper.RemoveNode(pair2.Node)
+			},
+		},
+		"pair 3": {
+			pair: pair3,
+			f: func() error {
+				return mapper.RemoveString(pair3.String)
+			},
+		},
+	}
+
+	for name := range testcases {
+		testcase := testcases[name]
+
+		t.Run(
+			name,
+			func(t *testing.T) {
+				err := testcase.f()
+				if err != nil {
+					t.Errorf("expected no error, got %+v", err)
+				}
+				if mapper.HasString(testcase.pair.String) {
+					t.Error("expected mapper not to contain string")
+				}
+				if mapper.HasNode(testcase.pair.Node) {
+					t.Error("expected mapper not to contain node")
+				}
+			},
+		)
+	}
+}
+
+func TestStringMappingRemovalErrs(t *testing.T) {
+	g := digraph.New()
+	n1, n2, n3 := g.NewNode(), g.NewNode(), g.NewNode()
+
+	mapper := uniquemappers.NewString()
+	mapper.Add(uniquemappers.StringNodePair{Node: n1, String: "foo"})
+
+	testcases := map[string]func() error{
+		"invalid node": func() error {
+			return mapper.RemoveNode(n2)
+		},
+		"invalid string": func() error {
+			return mapper.RemoveString("bar")
+		},
+		"invalid node (pair)": func() error {
+			return mapper.Remove(uniquemappers.StringNodePair{Node: n2, String: "foo"})
+		},
+		"invalid string (pair)": func() error {
+			return mapper.Remove(uniquemappers.StringNodePair{Node: n1, String: "baz"})
+		},
+		"invalid pair": func() error {
+			return mapper.Remove(uniquemappers.StringNodePair{Node: n3, String: "bax"})
+		},
+	}
+
+	for name := range testcases {
+		testcase := testcases[name]
+
+		t.Run(
+			name,
+			func(t *testing.T) {
+				err := testcase()
+				if err == nil {
+					t.Errorf("expected removal to fail")
+				}
+			},
+		)
+	}
+}
+
+func TestStringMapperExposesContents(t *testing.T) {
+	g := digraph.New()
+	n1, n2, n3 := g.NewNode(), g.NewNode(), g.NewNode()
+
+	mapper := uniquemappers.NewString()
+	mapper.Add(uniquemappers.StringNodePair{Node: n1, String: "foo"})
+	mapper.Add(uniquemappers.StringNodePair{Node: n2, String: "bar"})
+	mapper.Add(uniquemappers.StringNodePair{Node: n3, String: "baz"})
+
+	t.Run(
+		"strings",
+		func(t *testing.T) {
+			strings := mapper.Strings()
+      sort.Strings(strings)
+      expected := []string{"bar", "baz", "foo"}
+
+			if len(strings) != len(expected) {
+				t.Fatalf("expected %d strings, got %+v", len(expected), strings)
+			}
+      for i := range expected {
+        if expected[i] != strings[i] {
+          t.Errorf("expected to find '%s'", expected[i])
+        }
+      }
+		},
+	)
+	t.Run(
+		"nodes",
+		func(t *testing.T) {
+      nodes := mapper.Nodes()
+      if len(nodes) != 3 {
+        t.Fatalf("expected 3 nodes, got %d", len(nodes))
+      }
+      found := make(map[digraph.Node]struct{})
+      for i := range nodes {
+        if _, ok := found[nodes[i]]; ok {
+          t.Errorf("duplicate node")
+          continue
+        }
+        found[nodes[i]] = struct{}{}
+      }
+      for node := range found {
+        switch node {
+        case n1:
+        case n2:
+        case n3:
+        default:
+          t.Error("unknown node found")
+        }
+      }
+    },
+	)
+}
